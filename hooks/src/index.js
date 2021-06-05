29df612007d514820ca7c688bc42cfd9b3e1e940
Thiago Henrique Prochnow <thiprochnow@gmail.com>
2021-06-05 17:14:19 -0300

    useContext Part 1

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -4,6 +4,8 @@ import React from 'react'
 
 import App from './views/App'
 
+import DataContext, { data } from './data/DataContext'
+
 ReactDOM.render(
     <App />,
     document.getElementById('root')
